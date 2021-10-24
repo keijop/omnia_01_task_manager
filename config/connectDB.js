54911645534c165445ed90a40b57e12f7e31b40b
keijop <76419484+keijop@users.noreply.github.com>
2021-10-24 10:43:38 +0300

    jawsDB connection added

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,14 +1,22 @@
 require('dotenv').config({ path: './.env' })
 const mysql = require('mysql');
 
- 
+let connection
 
-const connection = mysql.createConnection({
-                             host: process.env.DB_HOST, 
-                             user: process.env.DB_USER, 
-                             password: process.env.DB_PASSWORD, 
-                             database: process.env.DB_DATABASE
-                              });
+if(process.env.JAWSDB_URL){
+
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+} else{
+
+  connection = mysql.createConnection({
+                               host: process.env.DB_HOST, 
+                               user: process.env.DB_USER, 
+                               password: process.env.DB_PASSWORD, 
+                               database: process.env.DB_DATABASE
+                                });
+}
+
 
 connection.connect((err) => {
   if(err){
